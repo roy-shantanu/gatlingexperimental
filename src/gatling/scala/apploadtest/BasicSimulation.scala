d297cfb0ba9420fd95631597c8383d6503392493
shantanu <shantanu.roy@ideascale.com>
2018-05-10 19:47:15 +0600

    First commit

---
+++ src/gatling/scala/apploadtest/BasicSimulation.scala
@@ -0,0 +1,30 @@
+package apploadtest
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+/**
+  * User: shantanu 
+  * Date: 5/10/18
+  * Time: 6:08 PM
+  */
+class BasicSimulation extends Simulation {
+
+  val httpConf = http // 4
+    .baseURL("http://computer-database.gatling.io") // 5
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // 6
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+
+  val scn = scenario("BasicSimulation") // 7
+    .exec(http("request_1") // 8
+    .get("/")) // 9
+    .pause(5) // 10
+
+  setUp(// 11
+    scn.inject(atOnceUsers(1)) // 12
+  ).protocols(httpConf) // 13
+
+}
